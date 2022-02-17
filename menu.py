f80cc3db75046e24dd2622ed6bc949235f1236d2
Mr-Anyone <53135664+Mr-Anyone@users.noreply.github.com>
2022-02-17 17:44:12 -0500

    my implementation of a menu

--- menu.py
+++ menu.py
@@ -1,29 +1,33 @@
-import urwid
+from fileinput import filename
+import urwid 
 import logging 
 logging.basicConfig(filename="log.log")
 
-choices = u'Chapman Cleese Gilliam Idle Jones Palin'.split()
 
-def menu(title, choices):
-    body = [urwid.Text(title, align="center"), urwid.Divider()]
-    for c in choices:
-        button = urwid.Button(c)
-        urwid.connect_signal(button, 'click', item_chosen, c) # Clicked or something
+choices = "Apple Banna Grape".split() 
+def exit_on_q(key):
+    if key in ('q', "Q"):
+        raise urwid.ExitMainLoop() 
 
-        body.append(urwid.AttrMap(button, None, focus_map='reversed'))
-    return urwid.ListBox(urwid.SimpleFocusListWalker(body))
+def menu(choices):
+    body = []
+    for choice in choices: 
+        button = urwid.Button(choice)
+        urwid.connect_signal(button, 'click', on_click)
+        body.append(button)
+    
+    return urwid.ListBox(body)
 
-def item_chosen(button, choice):
-    response = urwid.Text(f"You have choosen {choice}! ") 
-    button = urwid.Button("Done")
-    urwid.connect_signal(button, 'click', exit_program)
-    main.original_widget = urwid.Filler(urwid.Pile([response, urwid.Divider(), button]))
+def on_click(button):
+    logging.log(f"Button was clicked {button}")
 
-def exit_program(button):
-    raise urwid.ExitMainLoop()
+palette = [ 
+    ('bg', 'dark blue', 'dark blue'), 
+]
 
+background = urwid.SolidFill("/")
+menu_view = menu(choices)
 
-
-main = urwid.Padding(menu(u'Pythons', choices), left=2, right=2)
-top = urwid.Overlay(main, urwid.SolidFill(u"\\"), 'center', ('relative', 70),'middle',  ('relative', 70))
-urwid.MainLoop(top, palette=[('reversed', 'standout', '')]).run()
+overlay_view = urwid.Overlay(menu_view, background, align='center',  width=('relative', 80), valign='middle', height=('relative', 80))
+loop = urwid.MainLoop(overlay_view, palette, unhandled_input=exit_on_q)
+loop.run()
